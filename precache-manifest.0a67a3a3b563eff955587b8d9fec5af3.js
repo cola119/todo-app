01379564b2b06b250f9df5b5b4e78b62885eba60
kouhei ueno <k.cola119@gmail.com>
2019-04-28 18:27:37 +0900

    Updates

---
+++ precache-manifest.0a67a3a3b563eff955587b8d9fec5af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfde78211652bddf58160adb2f1fcfd",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "3ca57d551e269e504611",
+    "url": "/todo-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "249586c1edf830bb8b66",
+    "url": "/todo-app/static/js/2.5b895f98.chunk.js"
+  },
+  {
+    "revision": "3ca57d551e269e504611",
+    "url": "/todo-app/static/js/main.46f11f08.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
